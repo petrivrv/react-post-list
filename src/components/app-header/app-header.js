c7a16a29426d761037af68fc2e78b82864df6ff7
vscode <vs@gmail.com>
2022-07-21 16:18:44 +0300

    JSX pract

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader =() => {
+    return (
+        <div className='app-header d-flex'>
+            <h1>Roman Petriv</h1>
+            <h2> 5 rows</h2>
+
+        </div>
+    )
+}
+export default AppHeader;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchPanel =() => {
+    return (
+        <input 
+            className='form-control search-input'
+            type='Text'
+            placeholder='Enter text...'
+        />
+        
+    )
+}
+export default SearchPanel;
